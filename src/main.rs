1ecfff47fb99479e220cb77b72786e64b1edc177
Eric Moy <ericzmoy@gmail.com>
2023-08-10 23:40:37 -0500

    Added fibonacci sequence

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use dialoguer::{Input, Select};
 
 fn main() {
-    temp_conv_seq();
+    // Main menu selection screen
+    let choices = vec!["Convert Temperature", "Nth term in Fibonacci Sequence"];
+    let user_input = Select::new()
+        .items(&choices)
+        .interact()
+        .unwrap();
+        
+    match user_input {
+        0 => temp_conv_seq(),
+        1 => fibonacci_sequence(),
+        _ => {
+            println!("Input Error, please try again!");
+            main()
+        }
+    }
 }
 
 // Sequences
 fn temp_conv_seq() {
     let items = vec!["Fahrenheit", "Celsius"];
     let type_input = Select::new()
+        .with_prompt("Select your input type")
         .items(&items)
         .interact()
         .unwrap();
@@ -28,6 +43,17 @@ fn temp_conv_seq() {
     // final_temp
 }
 
+fn fibonacci_sequence() {
+    let input: u32 = Input::new()
+    .with_prompt("Enter nth term")
+    .interact_text()
+    .unwrap();
+
+    let fib_value: u32 = fibonacci(input);
+
+    println!("{fib_value} is term number {input} in the Fibonacci sequence" )
+}
+
 // Helper functions
 fn fahrenheit_celcius(temp: f32, input_type: &str) -> (f32, &str) {
     if input_type == "Celsius" {
@@ -39,5 +65,13 @@ fn fahrenheit_celcius(temp: f32, input_type: &str) -> (f32, &str) {
     } else {
         println!("Invalid input type...");
         (0.0, "Error")
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n-1) + fibonacci(n-2)
     }
 }
